98acdf8ead27448e5cec8abcb962a9a09b3b7293
kaling <korinode@gmail.com>
2023-06-04 00:20:38 +0900

    지옥의 EXP 시스템

---
+++ Function/PointCount.js
@@ -0,0 +1,37 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('../angela-lob-firebase-adminsdk-rau52-2ff672c6bb.json');
+const { RandomMachine } = require('./Random');
+const { updateRanking } = require('./Ranking');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+const db = admin.firestore();
+
+function PointCount(message){
+  updateRanking(db, message);
+  const userRef = db.collection('users').doc(message.author.id);
+  // 데이터 조회하기
+  userRef.get().then(doc => {
+    if (doc.exists) {
+      const NowLevel = Math.floor(Math.sqrt(doc.data().exp))
+      if(!(doc.data().level === NowLevel)){//레벨 상승
+        userRef.update({
+          level: NowLevel
+        });
+        message.channel.send(`${doc.data().name}님 축하합니다. ${NowLevel}레벨이 되었습니다!`)
+      }
+
+		  userRef.update({
+		    exp: doc.data().exp + RandomMachine(),
+		  });
+    } else {
+      message.channel.send("!등록을 먼저 진행해주세요.")
+    }
+  });
+}
+
+module.exports = {
+  db: db,
+  PointCount: PointCount
+};
